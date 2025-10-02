ac0ecee3072270cc8fd10e3eb99d3c5b96f0df15
witbawilliam <witbawilliam@gmail.com>
2025-10-02 08:14:21 +0100

    Initail commit

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,7 +1,33 @@
+import mysql.connector
+
+from config import Config
+
+import logging
+
 class DBConnection:
     def __init__(self):
-        pass
+        self.connection = None
 
     # TODO: create a method to connect to the database using the database credentials from config class
     def connect(self):
-        pass
+        try:
+            self.connection = mysql.connector.connect(
+
+                host=Config.DB_HOST,
+
+                port=Config.DB_PORT,
+
+                password=Config.DB_PASSWORD,
+
+                user=Config.DB_USER,
+
+                dbname=Config.DB_NAME
+
+
+
+            )
+
+        except Exception as e:
+
+            logging.info(f"An error uccord during database connection: str({e})")
+
