c36faee9db81d591aebafbc20f8d984aeccc83cb
Zeh Brien <110848929+Brien123@users.noreply.github.com>
2025-11-06 06:04:41 +0100

    Revert "Adding pagination to the product detail page "

--- src/data/helper.py
+++ src/data/helper.py
@@ -1,29 +1,16 @@
 import logging
 from typing import Dict, Optional, Any, List
 import pandas as pd
-<<<<<<< HEAD
 from sqlalchemy import text, bindparam
 
-=======
-from sqlalchemy import text
->>>>>>> my-feature
 from src.database.connection import DBConnection
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-
 
 
 class Helper:
-    def __init__(self, db_manager: DBConnection):
+    def __init__(self):
         self._db_manager = DBConnection()
 
-<<<<<<< HEAD
     def get_product(self, product_id: int, ) -> Optional[Dict[str, Any]]:
-=======
-    def get_product(self, product_id, int) -> Optional[Dict[str, Any]]:
->>>>>>> my-feature
         query = """
             SELECT * FROM products
             WHERE id = :product_id;
@@ -49,13 +36,12 @@ class Helper:
             self._db_manager.close(db_connection)
 
         return product_data
-    
 
-    def get_products(self, int) -> Optional[List[Dict[str, Any]]]:
+    def get_products(self) -> Optional[List[Dict[str, Any]]]:
         query = "SELECT * FROM products"
         db_connection = self._db_manager.connect()
         try:
-            products_df: pd.DataFrame = pd.read_sql(text(query))
+            products_df: pd.DataFrame = pd.read_sql(text(query), con=db_connection)
             products_list = products_df.to_dict(orient="records")
         except Exception as e:
             logging.error(f"Error getting products: {e}")
@@ -65,7 +51,6 @@ class Helper:
 
         return products_list
     
-<<<<<<< HEAD
     def get_product_by_category(self, category_id: int,) -> Optional[List[Dict[str, Any]]]:
         query = """
             SELECT * FROM products
@@ -148,122 +133,3 @@ class Helper:
             self._db_manager.close(db_connection)
 
         return product_list
-=======
-
-
-
-    def get_product_id(self, user_id: int) -> Optional[Dict[str, Any]]:
-
-        query = "SELECT * FROM product  WHERE  user_id = user_id "
-
-        params = {"user_id":user_id}
-
-        db_engine = self._db_manager.get_engine()
-
-        try:
-            product_df: pd.DataFrame = pd.read_sql(
-
-                sql=query,
-
-                con=db_engine,
-
-                params=params
-            )
-            if not product_df.empty:
-
-                product_list = product_df.to_dict(orient="records")
-
-                return product_list
-
-            else:
-
-                return []
-
-        except Exception as e:
-
-            logging.error(f"Error getting product: {e}")
-
-            return []
-        
-
-
-
-
-    def  get_product_category(self, caterory_id: int) -> list[Dict[str, Any]]:
-
-        query = "SELECT * FROM product WHERE category_id = category_id"
-
-        params = {"category_id":caterory_id}
-
-        db_ingine = self._db_manager.get_engine()
-
-        try:
-
-            product_df: pd.DataFrame = pd.read_sql(text(query), con=db_ingine, params=params)
-
-            if not product_df.empty:
-
-                product_list = product_df.to_dict(orient="records")
-
-                return product_list
-            
-            else:
-
-                return []
-            
-        except Exception as e:
-
-            logging.error(f"Error can not get product py category: {e}")
-
-            return []
-
-
-
-
-
-    def get_product_date(self, created_at: int) -> List[Dict[str, Any]]:
-
-        query = "SELECT * FROM products WHERE created_at = created_at"
-
-        params = {"created_at": created_at}
-
-        db_engine = self._db_manager.get_engine()
-
-        try:
-                product_df: pd.DataFrame = pd.read_sql(sql=query, con=db_engine, params=params)
-
-                if not product_df.empty:
-
-                    product_list = product_df.to_dict(orient="records")
-
-                    return product_list
-
-                else:
-
-                    return []
-
-        except Exception as e:
-                
-            logging.error(f"Error to get products by date: {e}")
-
-
-            return []
-
-        
-
-
-
-
-
-
-
-
-
-
-
-     
-
-
-
-   
->>>>>>> my-feature
--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -1,19 +1,6 @@
 from src.data.helper import Helper
 
+helper = Helper()
 
- 
-
-#  instance of my DBConnection 
-db_manager = DBConnection()
-
-# An instance of Helper to pass the db_manager to it
-helper = Helper(db_manager)
-
-#  Call the method 
-products = helper.get_product_category(4)
-
+products = helper.get_products()
 print(products)
-
-
-
-
